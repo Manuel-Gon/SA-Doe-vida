ebaf2106550db41cb68ec69539103281e742db23
Josué de Oliveira Mendonça <kanji.flimorago@gmail.com>
2024-11-08 17:08:21 -0300

    Josué

--- doe vida/src/Router/Router.jsx
+++ doe vida/src/Router/Router.jsx
@@ -1,9 +1,6 @@
 import { createBrowserRouter } from "react-router-dom"; 
 import Doador_login from "../Doador/Doador_login";
-<<<<<<< HEAD
 import Doador_pefil from "../Doador/Doador_perfil";
-=======
->>>>>>> e70836603b2796ee11dbcfb6a6dca23211d982bd
 import Tela_doador from "../Doador/Tela_doador";
 import HospitalCadastro from "../Hospital/HospitalCadastro";
 import LoginHospital from "../Hospital/LoginHospital";
@@ -15,7 +12,7 @@ import Parceiros from "../telaparatodos/Parceiros";
 import QuemSomos from "../telaparatodos/QuemSomos";
 import TelaInicialT from "../telaparatodos/TelaInicialT";
 import Doador_cadastro from "../Doador/Doador_cadastro";
-
+import Sangue_doado from "../Doador/Sangue_doado";
 
 
 const router = createBrowserRouter([
@@ -29,13 +26,10 @@ const router = createBrowserRouter([
     {path: "/qsomos", element: <QuemSomos/>},
     {path: "/perfilH", element: <PerfilHospital/>},
     {path: "/loginH", element: <LoginHospital/>},
-<<<<<<< HEAD
     {path: "/perfilD", element: <Doador_pefil/>},
-=======
-    {path: "/perfilD", element: <Doador_perfil/>},
->>>>>>> e70836603b2796ee11dbcfb6a6dca23211d982bd
     {path: "/loginD", element: <Doador_login/>},
     {path: "/telaiDoador", element: <Tela_doador/>},
+    {path: '/sangueDoado', element: <Sangue_doado/>}
 ])
 
 export default router;
