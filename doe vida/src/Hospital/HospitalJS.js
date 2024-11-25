21fd153a65c37e756599a3ed69f6cb3e5f3fc502
SUYANE CONCEIÇÃO NOGUEIRA <suyane_nogueira@estudante.sesisenai.org.br>
2024-11-25 17:20:58 -0300

    Suyane

--- doe vida/src/Hospital/HospitalJS.js
+++ doe vida/src/Hospital/HospitalJS.js
@@ -6,7 +6,7 @@ const app = express();
 const pool = new Pool({
     user: 'Postgres', // Substitua pelo seu usuário do PostgreSQL
     host: 'localhost',
-    database: 'SA_Hospital_doeVida', // Nome da sua database
+    database: 'DoeVida_Hospital', // Nome da sua database
     password: 'senai', // Substitua pela sua senha
     port: 5432, // Porta padrão do PostgreSQL
 });
@@ -16,37 +16,37 @@ app.use(cors());
 app.use(express.json());
 
 // Rota para buscar todos os clientes
-app.get('/clientes', async (req, res) => {
+app.get('/Usuarios', async (req, res) => {
     try {
-        const result = await pool.query('SELECT * FROM clientes');
+        const result = await pool.query('SELECT * FROM Usuarios');
         res.json(result.rows);
     } catch (err) {
         console.error(err.message);
-        res.status(500).json({ error: 'Erro ao buscar clientes' });
+        res.status(500).json({ error: 'Erro ao buscar o Hospital' });
     }
 });
 
 // Rota para buscar um cliente por ID
-app.get('/clientes/:id', async (req, res) => {
+app.get('/Usuarios/:id', async (req, res) => {
     const { id } = req.params;
     try {
-        const result = await pool.query('SELECT * FROM clientes WHERE id = $1', [id]);
+        const result = await pool.query('SELECT * FROM Usuarios WHERE id = $1', [id]);
         if (result.rows.length === 0) {
-            return res.status(404).json({ error: 'Cliente não encontrado' });
+            return res.status(404).json({ error: 'hospital não encontrado' });
         }
         res.json(result.rows[0]);
     } catch (err) {
         console.error(err.message);
-        res.status(500).json({ error: 'Erro ao buscar cliente' });
+        res.status(500).json({ error: 'Erro ao buscar Hospital' });
     }
 });
 
 // Rota para adicionar um cliente
-app.post('/clientes', async (req, res) => {
+app.post('/CadastroHospital', async (req, res) => {
     const { nome, endereco, email, telefone } = req.body;
     try {
         const result = await pool.query(
-            'INSERT INTO clientes (nome, endereco, email, telefone) VALUES ($1, $2, $3, $4) RETURNING *',
+            'INSERT INTO CadastroHospital (nome, endereco, email, telefone) VALUES ($1, $2, $3, $4) RETURNING *',
             [nome, endereco, email, telefone]
         );
         res.status(201).json(result.rows[0]);
