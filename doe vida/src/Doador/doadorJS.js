e37a4f20464f58246ead8bad48ba61b16127a204
SuyaneNogueira <naolhenaofale7@gmail.com>
2024-11-28 15:44:10 -0300

    Suyane

--- doe vida/src/Doador/doadorJS.js
+++ doe vida/src/Doador/doadorJS.js
@@ -18,31 +18,31 @@ app.use(express.json());
 // Rota para buscar todos os clientes
 app.get('/UsuariosDoador', async (req, res) => {
     try {
-        const result = await pool.query('SELECT * FROM clientes');
+        const result = await pool.query('SELECT * FROM UsuariosDoador');
         res.json(result.rows);
     } catch (err) {
         console.error(err.message);
-        res.status(500).json({ error: 'Erro ao buscar clientes' });
+        res.status(500).json({ error: 'Erro ao buscar Doador' });
     }
 });
 
 // Rota para buscar um cliente por ID
-app.get('/clientes/:id', async (req, res) => {
+app.get('/UsuariosDoador/:id', async (req, res) => {
     const { id } = req.params;
     try {
-        const result = await pool.query('SELECT * FROM clientes WHERE id = $1', [id]);
+        const result = await pool.query('SELECT * FROM UsuariosDoador WHERE id = $1', [id]);
         if (result.rows.length === 0) {
-            return res.status(404).json({ error: 'Cliente não encontrado' });
+            return res.status(404).json({ error: ' Doador não encontrado' });
         }
         res.json(result.rows[0]);
     } catch (err) {
         console.error(err.message);
-        res.status(500).json({ error: 'Erro ao buscar cliente' });
+        res.status(500).json({ error: 'Erro ao buscar Doador' });
     }
 });
 
 // Rota para adicionar um cliente
-app.post('/clientes', async (req, res) => {
+app.post('/CadastroDoador', async (req, res) => {
     const { nome, endereco, email, telefone } = req.body;
     try {
         const result = await pool.query(
