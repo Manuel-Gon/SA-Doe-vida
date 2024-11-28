c3661fcf9e00f080d3a3ea74b0465091c1435ea3
Josué de Oliveira Mendonça <kanji.flimorago@gmail.com>
2024-11-28 16:29:09 -0300

    Josué

--- doe vida/src/Doador/Doador_login.jsx
+++ doe vida/src/Doador/Doador_login.jsx
@@ -24,33 +24,33 @@ function Doador_login() {
   return (
     <div className='container-login-doador'>
       <div className='imagem-login-doador'>
-        <img src="./tela_login_doador.png" alt="Imagem de Login" />
+        <img src="../../public/tela_login_doador.png" alt="Imagem de Login" id='imagem-de-login' />
       </div>
 
+      <form className='formulario-login-doador' onSubmit={e => { e.preventDefault(); handleLogin(); }}>
       <div className='informacoes-login-doador'>
         <h1>Login</h1>
         <p>Digite seus dados de acesso no campo abaixo.</p>
       </div>
-
-      <form className='formulario-login-doador' onSubmit={e => { e.preventDefault(); handleLogin(); }}>
+      <br /><br />
         <div className='botao-voltar-container-login-doador'>
           <button className='botao-voltar-login-doador' type="button" onClick={handleBack}>Voltar</button>
         </div>
-
+<br /><br /><br />
         <div className='Requisitos-login-doador'>
-          <label htmlFor="email">E-mail</label>
+          <label htmlFor="email">E-mail</label><br /><br /><br />
           <input id="email" type="email" name="email" placeholder="Digite seu e-mail" required />
         </div>
-
+<br />
         <div className='Requisitos-login-doador'>
-          <label htmlFor="password">Senha</label>
+          <label htmlFor="password">Senha</label><br /><br /><br />
           <input id="password" type="password" name="password" placeholder="Digite sua senha" required />
         </div>
-
+<br /><br />
         <div className='esqueceuSenha'>
           <button type="button" onClick={handleForgotPassword}>Esqueci minha senha</button>
         </div>
-
+<br /><br />
         <div className='termos-e-politicas-login-doador'>
           <input type="checkbox" id="confirmacao" name="confirmacao" required />
           <label htmlFor="confirmacao">
@@ -59,11 +59,11 @@ function Doador_login() {
             <button type="button" onClick={() => window.open('/politica', '_blank')}>Política de Privacidade</button>.
           </label>
         </div>
-
+<br />
         <div className='botao-entrar-login-doador'>
           <button type="submit">Entrar</button>
         </div>
-
+<br />
         <div className='naoPossuiConta'>
           <label>Não possui conta? <button type="button" onClick={handleRegister}>Cadastre-se</button></label>
         </div>
